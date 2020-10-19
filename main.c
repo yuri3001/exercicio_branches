97bacc4ad920227dc44d9f9352ee9c7d31a67dad
Yuri Braga <ybr2038@gmail.com>
2020-10-19 20:50:48 -0300

    adcionando funcionalidade ao programa

--- main.c
+++ main.c
@@ -3,6 +3,12 @@
 
 int main()
 {
-    int n1
+    float n1,n2;
+    printf("Seja bem-vindo ao meu programa que faz soma\n");
+    printf("Digite um numero: ");
+    scanf("%f",&n1);
+    printf("\nDigite um outro numero: ");
+    scanf("%f",&n2);
+    printf("O resultado:  %.2f",n1+n2);
         return 0;
 }
